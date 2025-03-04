9779d6f9736d52b9b1c25a0435bfc7d3b58f69db
Erhard Pfisztner <e.pfisztner@gmail.com>
2025-03-04 14:33:07 +0100

    Remove Eclipse and Flutter configuration files along with related platform-specific resources
    This commit deletes generated configuration files for Eclipse (.classpath, .project) and Flutter (platform-specific .gitignore files, manifests, and build files). It also removes platform-specific build-related resources (e.g., CMakeLists.txt and gradle files). This cleanup simplifies the repository by removing redundant or unused build configurations and resources.

--- lib/services/bluetooth_service.dart
+++ lib/services/bluetooth_service.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:blue_thermal_printer/blue_thermal_printer.dart' as thermal;
+import 'package:print_bluetooth_thermal/print_bluetooth_thermal.dart' as winThermal;
 import '../pos_printer_manager.dart';
 
 class BluetoothService {
@@ -20,31 +21,20 @@ class BluetoothService {
             ),
           )
           .toList();
-    }
-    //  else if (Platform.isIOS) {
-    // fblue.FlutterBlue flutterBlue = fblue.FlutterBlue.instance;
-    // var results = <fblue.BluetoothDevice>[];
-    // await flutterBlue.startScan(timeout: Duration(seconds: 10));
+    } else if (Platform.isWindows) {
+      final List<winThermal.BluetoothInfo> listResult = await winThermal.PrintBluetoothThermal.pairedBluetooths;
 
-    // flutterBlue.scanResults.listen((stream) {
-    //   for (fblue.ScanResult result in stream) {
-    //     results.add(result.device);
-    //   }
-    // });
-    // await flutterBlue.stopScan();
-    // devices = results
-    //     .toSet()
-    //     .toList()
-    //     .map(
-    //       (d) => BluetoothPrinter(
-    //         id: d.id.id,
-    //         address: d.id.id,
-    //         name: d.name,
-    //         type: d.type.index,
-    //       ),
-    //     )
-    //     .toList();
-    // }
+      devices = listResult
+          .map(
+            (d) => BluetoothPrinter(
+          id: d.macAdress,
+          address: d.macAdress,
+          name: d.name,
+          type: 0,
+        ),
+      )
+          .toList();
+    }
     return devices;
   }
 }
